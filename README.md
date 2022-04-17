0f170df0af2716841bcdffce958b35d9d7ff6f9a
Go Suzuki <puyogo.suzuki@gmail.com>
2022-04-17 22:16:40 +0900

    [ADD] Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # formura0-test-rungekutta2022
 Formura0 Test Computation Codes for checking effect of temporary variables (2022).
+
+## requirements
+Clang or Visual C++ or GCC, C compiler compatible common set of C99 and C18.
+
+For visualization, I used scilab and animaGIF.
+
+## What this does?
+Check performance and correctness of Temporary Grid Variable of Formura0.  
+Temporary Grid Variable may make performance improvements on multistep methods such as Runge-kutta method.  
+
+## Structure
+This repository contains diffusion equation code using runge-kutta method.
+ - rk.tmp.fmr : With Temporary Grid Variable
+ - tmpmain.c : C Code Output with Temporary Grid Variable
+ - tmpmain.csv : Output of tmpmain.c
+ - tmpmain.gif : Visualized Gif Animation of tmpmain.csv
+ - rk.notmp.fmr : Without Temporary Grid Variable
+ - notmpmain.c : C Code Output without Temporary Grid Variable
+ - notmpmain.csv : Output of notmpmain.c
+ - notmpmain.gif : Visualized Gif Animation of notmpmain.csv
+
+## Result
+I tested Visual C++ 19.29.30142.1 for x64 and Ryzen 5850U, 32GiB machine, and /O2 command line option.  
+TGV means Temporary Grid Variable.
+| # | with TGV | without TGV |
+--------------------------------
+| 1 |  60 sec  |   98 sec    |
+| 2 |  61 sec  |   98 sec    | 
---
+++ notmpmain.c
@@ -0,0 +1,115 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+#define TRUE 1
+#define FALSE 0
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+#define MAX_I1 100
+
+
+typedef uint64_t formura_time_t;
+
+typedef struct Formura0Navi
+{
+  formura_time_t time;
+  double* q;
+  double* q_prev;
+
+
+} Formura0Navi;
+
+
+
+void formura_step(Formura0Navi* data)
+{
+  //--body start--
+  for(int i1 = 5;i1 < 100; ++i1) {
+    double localVal_0 = data->q_prev[i1];
+    double localVal_1 = data->q_prev[(i1 + -1)];
+    int32_t localVal_6 = 100.0;
+    double localVal_10 = (localVal_1 - localVal_0) * 1.0e-3 / 1.0e-2;
+    double localVal_14 = data->q_prev[(i1 + -2)];
+    double localVal_19 = (localVal_1 + (localVal_14 - localVal_1) * 1.0e-3 / 1.0e-2 / 2.0);
+    double localVal_22 = (localVal_19 - (localVal_0 + localVal_10 / 2.0)) * 1.0e-3 / 1.0e-2;
+    double localVal_25 = data->q_prev[(i1 + -3)];
+    double localVal_30 = (localVal_14 + (localVal_25 - localVal_14) * 1.0e-3 / 1.0e-2 / 2.0);
+    double localVal_35 = (localVal_1 + (localVal_30 - localVal_19) * 1.0e-3 / 1.0e-2 / 2.0);
+    double localVal_38 = (localVal_35 - (localVal_0 + localVal_22 / 2.0)) * 1.0e-3 / 1.0e-2;
+    data->q[i1] = (localVal_0 + (((localVal_10 / 6.0 + localVal_22 / 3.0) + localVal_38 / 3.0) + ((localVal_1 + ((localVal_14 + ((localVal_25 + (data->q_prev[(i1 + -4)] - localVal_25) * 1.0e-3 / 1.0e-2 / 2.0) - localVal_30) * 1.0e-3 / 1.0e-2 / 2.0) - localVal_35) * 1.0e-3 / 1.0e-2) - (localVal_0 + localVal_38)) * 1.0e-3 / 1.0e-2 / 6.0));
+  }
+  //--body end--//--body start--
+  for(int i1 = 0;i1 < 5; ++i1) {
+    data->q[i1] = 1.0;
+  }
+  //--body end--
+}
+
+
+
+
+
+
+
+int formura_initialize(Formura0Navi* data){
+  if((data->q = (double *)malloc(sizeof(double)*100)) == 0)
+    return 0;
+  if((data->q_prev = (double *)malloc(sizeof(double)*100)) == 0)
+    return 0;
+
+  return 1;
+}
+
+void formura_setup(Formura0Navi* data, char* dumpdata)
+{
+  data->time = 0;
+  //--body start--
+  for(int i1 = 0;i1 < 100; ++i1) {
+    data->q[i1] = (i1 > 20.0 ? 0.0 : 1.0);
+  }
+  //--body end--
+  {
+    double* tmp = data->q;
+    data->q = data->q_prev;
+    data->q_prev = tmp;
+  }
+
+}
+
+void formura_forward(Formura0Navi* data)
+{
+  formura_step(data);
+  {
+    double* tmp = data->q;
+    data->q = data->q_prev;
+    data->q_prev = tmp;
+  }
+
+  data->time++;
+}
+
+int main(void) {
+  Formura0Navi data;
+  if(!formura_initialize(&data)) {
+    printf("Allocation failed.\n");
+    return 0;
+  }
+  formura_setup(&data, NULL);
+/*  printf("%lf", data.q_prev[0]);
+  for(int q = 1; q < 100; ++q)
+    printf(",%lf", data.q_prev[q]);
+  printf("\n");*/
+  for(int i = 0; i < 100000000; ++i) {
+    formura_forward(&data);
+/*    printf("%lf", data.q_prev[0]);
+    for(int q = 1; q < 100; ++q)
+      printf(",%lf", data.q_prev[q]);
+    printf("\n");*/
+  }
+  return 0;
+}
